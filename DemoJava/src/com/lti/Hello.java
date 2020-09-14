9c4ca9abd19003c1e897e342cbb25faf2bd6a863
rithikmisri <rithikmisri29@gmail.com>
2020-09-14 12:34:43 +0530

    added amigos

--- DemoJava/src/com/lti/Hello.java
+++ DemoJava/src/com/lti/Hello.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello amigos");
 		Date d = new Date();
 		System.out.println(d);
 		LocalDateTime dt = LocalDateTime.now();

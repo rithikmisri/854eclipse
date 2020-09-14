c120f50dc66f3910ef5b4b777e6c16c48d98ab39
rithikmisri <rithikmisri29@gmail.com>
2020-09-14 12:09:17 +0530

    another commit

--- DemoJava/src/com/lti/Hello.java
+++ DemoJava/src/com/lti/Hello.java
@@ -1,9 +1,13 @@
 package com.lti;
 
+import java.util.Date;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		Date d = new Date();
+		System.out.println(d);
 
 	}
 
